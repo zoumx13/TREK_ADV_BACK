3dd9f51ca8ec5b00d9843fcf7ef566b6b0b5c698
Faelco <114163753+Faelco@users.noreply.github.com>
2022-10-31 14:36:43 +0100

    controller user

---
+++ controllers/users_controller.js
@@ -0,0 +1,18 @@
+const Users_model = require("../models/users_model");
+
+const users = {
+    CreateUser: async (req, res) => {
+    },
+
+    EditUser: async (req, res) => {
+    }, 
+
+    DeleteUser: async (req, res) => {
+    },
+
+    
+
+
+
+};
+module.exports = users;
