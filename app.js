f87b04f26124d5d53fab5322394a484c64117727
Manuel Elhadide <manuel.elhadide@gmail.com>
2022-10-31 11:26:23 +0100

    First Test Commit Main Ultime

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+// const middlewareLogIn = require("./middleware/")
+
+// let cors = require("cors")
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Création de route ici
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
---
+++ routes/users.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+
+
+/* GET users listing. */
+router.post('/',)
+module.exports = router;
