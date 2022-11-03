ed8cb9359c5fec2540f78865c60a477bf53fe9db
Sebastien ROBELLO <=sebastien.robello@hotmail.fr>
2022-11-03 15:01:41 +0100

    routes getUser

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,7 @@ router.post("/signup", users.CreateUser);
 router.post("/signin", users.SignIn);
 router.get("/admin", middleware.authentication, users.Admin);
 router.get("/user", tokenMiddle.token, users.GetUser);
+
 /* GET users listing. */
 router.post("/");
 module.exports = router;
