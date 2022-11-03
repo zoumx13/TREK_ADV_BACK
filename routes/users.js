46689a2c543c6b9eacc94b59372c4bcc6ca10832
Elodie Adam <allouche.elodie@gmail.com>
2022-11-03 10:40:14 +0100

    TA-42 CheckToken Routes

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ const users = require("../controllers/usersController");
 
 router.post("/signup", users.CreateUser);
 router.post("/signin", users.SignIn);
+router.get("/checktoken", users.checkToken);
 /* GET users listing. */
 router.post("/");
 module.exports = router;
