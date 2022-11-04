f4e7e79114adc66871e653f428c45adbdd13add9
Adrien Hazoumé <adrien.hazoume@yahoo.fr>
2022-11-04 13:17:15 +0100

    commit

--- controllers/parcoursController.js
+++ controllers/parcoursController.js
@@ -26,16 +26,16 @@ const parcours = {
             // Ligne ci-dessous utilisable plus tard avec multer
             //  imgIllustration: req.file !== null ? "./uploads/parcours/" + fileName : "",
             niveauDifficulte: niveauDifficulte,
-            etape: [
-                {
-                    nomEtape: nomEtape,
-                    numeroEtape: numeroEtape,
-                    localisation: localisation,
-                    descriptionEtape: descriptionEtape,
-                    // Ligne ci-dessous utilisable plus tard avec multer
-                    //     imgIllustrationEtape: req.file !== null ? "./uploads/etapes/" + fileName : "",
-                },
-            ],
+            // etape: [
+            //     {
+            //         nomEtape: nomEtape,
+            //         numeroEtape: numeroEtape,
+            //         localisation: localisation,
+            //             descriptionEtape: descriptionEtape,
+            //         // Ligne ci-dessous utilisable plus tard avec multer
+            //         //     imgIllustrationEtape: req.file !== null ? "./uploads/etapes/" + fileName : "",
+            //     },
+            // ],
         });
 
         try {
@@ -122,7 +122,8 @@ const parcours = {
                     res.json(data);
                     console.log("parcours chargés", data);
                 }
-            }).select("nomParcours");
+            })
+            // }).select("nomParcours");
     },
 
     addStep: (req, res) => {
