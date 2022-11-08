533937ac57f3ed7c9a56b51bd7721b92384ccf27
Elodie Adam <allouche.elodie@gmail.com>
2022-11-08 15:33:54 +0100

    ajout d'une route post pour reservationuser

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,7 @@ const tokenMiddle = require("../middleware/token");
 router.post("/signup", users.CreateUser);
 router.post("/signin", users.SignIn);
 router.post("/createguide", middleware.authentication, users.CreateUser);
+router.post("/reservationuser", users.GetUser);
 router.get("/admin", middleware.authentication, users.Admin);
 router.get("/user", tokenMiddle.token, users.GetUser);
 router.get("/guide", middleware.authentication, users.GetUser);
