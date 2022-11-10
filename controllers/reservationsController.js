78dcea85f5966f785740733296180348ef7ee4c4
Sebastien ROBELLO <=sebastien.robello@hotmail.fr>
2022-11-10 14:19:51 +0100

    ajout resaUser

--- controllers/reservationsController.js
+++ controllers/reservationsController.js
@@ -1,50 +1,74 @@
 const parcoursSchema = require("../models/ParcoursSchema");
 
-const reservations = {
-    createReservations:  async (req, res) => {
-        const {
-            openResa,
-            maxClients,
-            dateReservation,
-            idGuide    
-        } = req.body;
-        try {
-               await parcoursSchema.findByIdAndUpdate(
-                { _id: req.params.id },
-    
+const reservation = {
+  createReservations: async (req, res) => {
+    const { openResa, maxClients, dateReservation, idGuide } = req.body;
+    try {
+      await parcoursSchema
+        .findByIdAndUpdate(
+          { _id: req.params.id },
+
+          {
+            $push: {
+              reservations: [
                 {
-                    $push: {
-                        reservations: [
-                            {
-                                openResa: openResa,
-                                maxClients: maxClients,
-                                dateReservation: dateReservation,
-                                idGuide: idGuide,
-                                // POUR SEB 
-                                // clients : [{
-                                //     idClient : idClient,
-                                //     etapeCompletee : [{
-                                //         nomEtape : nomEtape,
-                                //         date : date
-                                //     }],
-                                //     finished : finished
+                  openResa: openResa,
+                  maxClients: maxClients,
+                  dateReservation: dateReservation,
+                  idGuide: idGuide,
 
-                                // }]
-                            },
-                        ],
-                    },
-                })
-                .then((docs) => res.status(200).json(docs))
-                .catch((err)=> res.status(400).json(err))
-                
+                  // clients : [{
+                  //     idClient : idClient,
+                  //     etapeCompletee : [{
+                  //         nomEtape : nomEtape,
+                  //         date : date
+                  //     }],
+                  //     finished : finished
 
-
-        } catch(err) {
-            return res.status(400).send(err);
-
-       
-      }
+                  // }]
+                },
+              ],
+            },
+          }
+        )
+        .then((docs) => res.status(200).json(docs))
+        .catch((err) => res.status(400).json(err));
+    } catch (err) {
+      return res.status(400).send(err);
     }
-    }
+  },
+  userReservation: async (req, res) => {
+    try {
+      const user = req.body.userId;
+      const parcourId = req.body.parcoursId;
+      const resaId = req.body.resaId;
+      console.log(parcourId);
+      console.log(resaId);
+      console.log(user);
+      parcoursSchema.findById(parcourId, (err, docs) => {
+        // accéder à THE Resa grâce à un find dans la docs , mettre une () avec un param "resa" puis comparé l'id "spé" de la resa avec equals
+        const theResa = docs.reservations.find((resa) =>
+          resa._id.equals(req.body.resaId)
+        );
+        console.log(theResa);
+        if (!theResa) {
+          res.status(404).send("Resa not found");
+        } else {
+          theResa.clients = { idClient: user };
 
-module.exports = reservations
+          docs.save((err) => {
+            if (!err) {
+              res.status(200).send(docs);
+            } else {
+              res.status(500).send(err);
+            }
+          });
+        }
+      });
+    } catch (err) {
+      res.status(409).send(err);
+    }
+  },
+};
+
+module.exports = reservation;
--- routes/reservations.js
+++ routes/reservations.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const reservations = require("../controllers/reservationsController");
+const tokenMiddle = require("../middleware/token");
 
-// Route Resa 
-router.patch("/createReservations/:id", reservations.createReservations)
+// Route Resa
+router.patch("/createReservations/:id", reservations.createReservations);
 // router.delete("/deletereservations", reservations.deleteReservations)
 // router.patch("/modifyReservations", reservations.modifyReservations)
 // router.get("/", reservations.getReservations)
-
-
-
+router.post("/user", tokenMiddle.token, reservations.userReservation);
 
 module.exports = router;
